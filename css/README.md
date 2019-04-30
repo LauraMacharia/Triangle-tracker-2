ef3882fef83ba3c57e84ba7505a32fa5b72d7a5b
Laura Macharia <lauramacharia117@gmail.com>
2019-04-30 21:09:01 +0300

    added  a README file

---
+++ css/README.md
@@ -0,0 +1,21 @@
+# Triangle tracker
+#### By Laura Macharia
+## Description
+This application helps to identify whether a triangle is isosceles equilateral or scalene.
+## Setup/Installation Requirements
+* To use the up you require internet connection.
+## BDD
+* Imput numbers in the spaces.
+* If all the sides are equal, it will alert equilateral.
+* If two sides are equal, it will alert isosceles.
+* If none of the sides are equal, it will alert scalene.
+* If the sum of two sides is less than or equal to the remaining side, the application will alert not a triangle.
+## Known Bugs
+There are no known bugs yet.
+## Technologies Used
+To build this application I used HTML, CSS and Javascript
+## Support and contact details
+If there are any complications contact me on lauramacharia117@gmail.com
+### License
+Copyright (c) {2019} 
+  
--- script.js
+++ script.js
@@ -2,15 +2,26 @@ function triangleTracker() {
     var base = parseInt(document.getElementById('base').value);
     var height = parseInt(document.getElementById('height').value);
     var hypotenuse = parseInt(document.getElementById('hypotenuse').value);
-     if (base === height && base === hypotenuse && height === hypotenuse){
-     alert("equilateral");
+
+     if (base<=0 || height<=0 || hypotenuse<=0){
+     alert("Enter positive numbers");
      }
-  else if ( base !== height && height !== hypotenuse && hypotenuse !== base) {
-    alert("scalene");
+  else if (base + height <= hypotenuse || height + hypotenuse <= base || base + hypotenuse <= height) {
+    alert("NOT  a triangle");
   }
-  else if ( base == height || height ===hypotenuse || height ===hypotenuse) {
-    alert("isosceles");
+  else if (base + height <= hypotenuse || height + hypotenuse <= base || base + hypotenuse <= height) {
+    alert("NOT  a triangle");
   }
-  else ( alert("this is not a triangle"));
+  else if (isNaN(base) || isNaN(height) || isNaN(hypotenuse)){
+    alert("Oops please input values");
+  }
+  else if (base === height && base === hypotenuse){
+    alert("Equilateral");
+  }
+  else if (base === height || base === hypotenuse || height === hypotenuse){
+    alert("Isosceles");
+  }
+
+  else ( alert("Scalene"));
   }
   
